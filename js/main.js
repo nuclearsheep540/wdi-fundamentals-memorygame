cb011fcbda0dd555c3f07a0599fab7b687919040
Matt Davey <matt.davey540@me.com>
2019-08-07 11:07:12 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "king";
-var cardThree = "Queen";
-var cardFour = "king";
+cards = ['Queen','Queen','King','King'];
+cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne,cardTwo);
+console.log("User flipped "+cardsInPlay);
+if (cardsInPlay.length === 2 && cardOne === cardTwo)
+	{
+		alert('You found a match!');
+	}
+	else
+		alert('Sorry, try again.');
