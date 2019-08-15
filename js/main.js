503fd27b2595215e2cec3bceda41ebcbd3a29de0
Matt Davey <matt.davey540@me.com>
2019-08-15 14:05:53 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,76 @@
-var cards = ['Queen','Queen','King','King'];
+cards = [
+{
+rank:'queen',
+suit:'hearts',
+cardImage:'/Users/Matt/fundamentals/wdi-fundamentals-memorygame/images/queen-of-hearts.png'
+},
+{
+rank:'queen',
+suit:'diamons',
+cardImage:'/Users/Matt/fundamentals/wdi-fundamentals-memorygame/images/queen-of-diamonds.png'
+},
+{
+rank:'king',
+suit:'hearts',
+cardImage:'/Users/Matt/fundamentals/wdi-fundamentals-memorygame/images/king-of-hearts.png'
+},
+{
+rank:'king',
+suit:'diamonds',
+cardImage:'/Users/Matt/fundamentals/wdi-fundamentals-memorygame/images/king-of-diamonds.png'
+},
+];
+
 var cardsInPlay = [];
 
-function flipCard(cardId){
-	console.log("User flipped "+cards[cardId]);
-	cardsInPlay.push(cardId);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
 };
-flipCard(0);
-flipCard(2);
+createBoard();
 
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	//console.log(this);
+	this.setAttribute('src',cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	//if (cardsInPlay.length % 2 == 0)
+	if (cardsInPlay.length === 2)
+	{
+		setTimeout(checkForMatch, 250);
+	}
+	if (cardsInPlay.length === 4) {
+		setTimeout(resetGame, 1000);
+	}
+};
+
+function checkForMatch (){
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!")} 
+	alert("You found a match!");} 
 else {
-	console.log("Try again!")}
+	console.log("bad match, reloading page")
+	alert("Try again!");
+	location.reload()};
+}
+
+function resetGame(){
+	console.log("game finished, check for user input")
+		if (confirm('You Win! \r\n\r\nPlay Again?')) {
+			console.log("reloading page... please wait...")
+			setTimeout(location.reload.bind(window.location),1500);
+		}
+		else {
+			console.log("user cancelled reload")
+			console.log("game has finished")
+			return;
+		}
+	}
+
+
+
+
